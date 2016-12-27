7332466f909e6072a842a98beffbc75fe740b5b7
Tom Molesworth <TEAM@cpan.org>
2016-12-27 20:34:06 +0800

    Update year

--- lib/Net/Async/Statsd/Server.pm
+++ lib/Net/Async/Statsd/Server.pm
@@ -217,5 +217,5 @@ Tom Molesworth <TEAM@cpan.org>
 
 =head1 LICENSE
 
-Copyright Tom Molesworth 2014-2015. Licensed under the same terms as Perl itself.
+Copyright Tom Molesworth 2014-2016. Licensed under the same terms as Perl itself.
 
